f0bec2ecaf69959e661a46519f6af2439ce3fa77
stacy.yeh <stacy.yeh@advantech.com.tw>
2017-12-15 14:42:10 +0800

    [Added] ScadaDao - checkScadaRightByUserName [Changed] - getScadaList - getScadaListByProjectId - deleteScada - bindScadas [Fixed] - updateScada [Removed] - unbindScadas

--- dao/scadaDao.js
+++ dao/scadaDao.js
@@ -1,50 +1,125 @@
 'use strict';
 
 const Promise = require('bluebird');
-const Sequelize = require('sequelize');
 const Utils = require('../common/utils');
+var squel = require('squel').useFlavour('postgres');
 
 var scadaVo = null;
-var deviceDao = require('./deviceDao');
+var userAllowDeviceVo = null;
+var deviceVo = null;
 var tagDao = require('./tagDao');
+var _sequelize = null;
 
 function _init (sequelize) {
   scadaVo = sequelize.import('../models/scadaVo');
-}
-
-function _getScadaList () {
-  return scadaVo.findAll();
+  deviceVo = sequelize.import('../models/deviceVo');
+  userAllowDeviceVo = sequelize.import('../models/userAllowDeviceVo');
+  _sequelize = sequelize;
 }
 
 /**
- * @param {String} projectId: projectId
- * @param {Object} params
- * @param {Integer} params.offset: starting index
- * @param {Integer} params.limit: data retrived
- * @param {String} params.scadaName: filter project name
- * @param {String} params.scadaDesc: filter project desc
- * @param {String} params.sortby: sort properties
- * @param {String} params.order: order asc or desc
+ * @param {Object} filterObj
+ * @param {Integer} filterObj.offset: starting index
+ * @param {Integer} filterObj.limit: data retrived
+ * @param {String} filterObj.scadaName: filter scada name
+ * @param {String} filterObj.description: filter scada desc
+ * @param {String} filterObj.sortby: sort properties
+ * @param {String} filterObj.order: order asc or not
+ * @param {Boolean} filterObj.detail: select id & name only
+ * @param {String} filterObj.userName: filter the scadas that userName can access
+ *  }
  */
-function _getScadaListByProjectId (projectId, params) {
-  let props = Object.keys(scadaVo.attributes);
-  let where = { projectId };
-  for (let idx in props) {
-    let key = props[idx];
-    if (!Utils.isNullOrUndefined(params[key])) {
-      where[key] = { $like: params[key] };
+function _getScadaList (filterObj = {}) {
+  let offset = filterObj.offset ? filterObj.offset : 0;
+  let limit = filterObj.limit ? filterObj.limit : null;
+  let sortby = filterObj.sortby ? filterObj.sortby : 'scadaId';
+  let order = filterObj.order ? filterObj.order : true;
+  let detail = filterObj.detail ? filterObj.detail : false;
+
+  let sql = squel.select().from('scada.scada_list', 'Scada');
+  if (detail) {
+    for (let idx in scadaVo.attributes) {
+      sql.field('Scada.' + scadaVo.attributes[idx].field, idx);
+    }
+  } else {
+    sql.field('Scada.proj_id', 'projectId');
+    sql.field('Scada.scada_id', 'scadaId');
+    sql.field('Scada.scada_name', 'scadaName');
+  }
+  sql.distinct();
+  if (filterObj.userName) {
+    sql.join('scada.user_allow_device', 'UserAllowDevice', squel.expr().and('Scada.proj_id = UserAllowDevice.proj_id').and('Scada.scada_id = UserAllowDevice.scada_id'));
+    sql.join('scada.user_info', 'UserInfo', squel.expr().and('UserAllowDevice.user_id = UserInfo.user_id'));
+    sql.where('Userinfo.user_name = ? ', filterObj.userName);
+  }
+  for (let idx in scadaVo.attributes) {
+    let key = scadaVo.attributes[idx];
+    if (!Utils.isNullOrUndefined(filterObj[idx])) {
+      sql.where('Scada.' + key.field + ' LIKE ?', filterObj[idx]);
     }
   }
-  let filter = {};
-  filter.offset = !Utils.isNullOrUndefined(params.offset) ? params.offset : null;
-  filter.limit = !Utils.isNullOrUndefined(params.limit) ? params.limit : null;
-  filter.order = !Utils.isNullOrUndefined(params.sortby) && !Utils.isNullOrUndefined(params.order) ? Sequelize.literal(params.sortby + ' ' + params.order) : null;
-  filter.where = where;
-  return scadaVo.findAndCountAll(filter);
+  sql.order(sortby, order);
+  return new Promise((resolve, reject) => {
+    _sequelize.query(sql.toString(), { type: _sequelize.QueryTypes.SELECT, model: scadaVo }).then((raws) => {
+      resolve({count: raws.length, rows: raws.slice(offset, limit ? limit + offset : raws.length)});
+    }).catch((err) => {
+      reject(err);
+    });
+  });
 }
 
-function _getScadaByProjectId (projectId, scadaId) {
-  return scadaVo.findOne({ where: { projectId, scadaId } });
+/**
+ * @param {String} projectId
+ * @param {Object} filterObj
+ * @param {Integer} filterObj.offset: starting index
+ * @param {Integer} filterObj.limit: data retrived
+* @param {String} filterObj.scadaName: filter scada name
+ * @param {String} filterObj.description: filter scada desc
+ * @param {String} filterObj.sortby: sort properties
+ * @param {String} filterObj.order: order asc or not
+ * @param {Boolean} filterObj.detail: select id & name only
+ * @param {String} filterObj.userName: filter the scadas that userName can access
+ *  }
+ */
+function _getScadaListByProjectId (projectId, filterObj = {}) {
+  let offset = filterObj.offset ? filterObj.offset : 0;
+  let limit = filterObj.limit ? filterObj.limit : null;
+  let sortby = filterObj.sortby ? filterObj.sortby : 'scadaId';
+  let order = filterObj.order ? filterObj.order : true;
+  let detail = filterObj.detail ? filterObj.detail : false;
+
+  let sql = squel.select().from('scada.scada_list', 'Scada');
+  if (detail) {
+    for (let idx in scadaVo.attributes) {
+      sql.field('Scada.' + scadaVo.attributes[idx].field, idx);
+    }
+  } else {
+    sql.field('Scada.proj_id', 'projectId');
+    sql.field('Scada.scada_id', 'scadaId');
+    sql.field('Scada.scada_name', 'scadaName');
+  }
+  sql.distinct();
+  if (filterObj.userName) {
+    sql.join('scada.user_allow_device', 'UserAllowDevice', squel.expr().and('Scada.proj_id = UserAllowDevice.proj_id').and('Scada.scada_id = UserAllowDevice.scada_id'));
+    sql.join('scada.user_info', 'UserInfo', squel.expr().and('UserAllowDevice.user_id = UserInfo.user_id'));
+    sql.where('Userinfo.user_name = ?', filterObj.userName);
+  }
+  sql.where('Scada.proj_id = ? ', projectId);
+
+  for (let idx in scadaVo.attributes) {
+    let key = scadaVo.attributes[idx];
+    if (!Utils.isNullOrUndefined(filterObj[idx])) {
+      sql.where('Scada.' + key.field + ' LIKE ?', filterObj[idx]);
+    }
+  }
+  sql.order(sortby, order);
+  return new Promise((resolve, reject) => {
+    _sequelize.query(sql.toString(), { type: _sequelize.QueryTypes.SELECT, model: scadaVo }).then((raws) => {
+      resolve({count: raws.length, rows: raws.slice(offset, limit ? limit + offset : raws.length)});
+    }).catch((err) => {
+      reject(err);
+    });
+  });
 }
 
 function _getScada (scadaId) {
@@ -55,48 +130,66 @@ function _insertScada (scada, trans) {
   return scadaVo.create(scada, { transaction: trans });
 }
 
-function _updateScada (scada, scadaId, trans) {
-  return scadaVo.update(scada, { where: { scadaId } }, { transaction: trans });
+function _updateScada (scadaId, scada, trans) {
+  return scadaVo.update(scada, { where: { scadaId }, transaction: trans });
 }
 
 function _deleteScada (scadaId, trans) {
   let promises = [];
   promises.push(scadaVo.destroy({ where: { scadaId }, transaction: trans }));
-  promises.push(deviceDao.deleteDeviceListByScadaId(scadaId, trans));
+  promises.push(deviceVo.destroy({ where: { scadaId }, transaction: trans }));
   promises.push(tagDao.deleteTagListByScadaId(scadaId, trans));
+  promises.push(userAllowDeviceVo.destroy({ where: { scadaId }, transaction: trans }));
   return Promise.all(promises);
 }
 
-function _unbindScadas (scadaIds = [], trans) {
+/* function _unbindScadas (scadaIds = [], trans) {
   let promises = [];
   for (let idx in scadaIds) {
     promises.push(scadaVo.update({projectId: null}, {where: {scadaId: scadaIds[idx]}}, { transaction: trans }));
   }
   return Promise.all(promises);
-}
+} */
 
 /**
- * 
+ *
  * @param {Array} array: Object of Array
  * @param {String} array[idx].projectId: project Id
  * @param {String} array[idx].scadaId: scada Id
- * @param {*} trans 
+ * @param {*} trans
  */
 function _bindScadas (array = [], trans) {
   let promises = [];
   for (let idx in array) {
-    promises.push(scadaVo.update({projectId: array[idx].projectId}, {where: {scadaId: array[idx].scadaId}}, { transaction: trans }));
+    promises.push(scadaVo.update({projectId: array[idx].projectId}, { where: {scadaId: array[idx].scadaId}, transaction: trans }));
+    promises.push(userAllowDeviceVo.update({projectId: array[idx].projectId}, {where: {scadaId: array[idx].scadaId}, transaction: trans}));
   }
   return Promise.all(promises);
 }
 
-function _isScadaIdsAllExist (scadaIds = [], trans) {
-  let where = {scadaId: {$or: scadaIds}};
+function _checkScadaRightByUserName (userName, filterObj = {}) {
+  let sql = squel.select().from('scada.scada_list', 'Scada');
+  sql.field('Scada.proj_id', 'projectId');
+  sql.field('Scada.scada_id', 'scadaId');
+  sql.field('SUB.userId', 'userId');
+  sql.distinct();
+  sql.left_join(squel.select().from('scada.user_allow_device', 'UserAllowDevice')
+  .field('UserAllowDevice.user_id', 'userId').field('UserAllowDevice.scada_id', 'scadaId')
+  .join('scada.user_info', 'UserInfo', squel.expr().and('UserAllowDevice.user_id = UserInfo.user_id').and('Userinfo.user_name = ?', userName)), 'SUB', 'SUB.scadaId = Scada.scada_id');
+  if (filterObj.projectId) {
+    sql.where(('UserAllowDevice.proj_id = ?', filterObj.projectId));
+  }
+  if (filterObj.scadaId) {
+    sql.where(('UserAllowDevice.scada_id = ?', filterObj.scadaId));
+  }
+  if (filterObj.deviceId) {
+    sql.where(('UserAllowDevice.device_id = ?', filterObj.deviceId));
+  }
   return new Promise((resolve, reject) => {
-    scadaVo.findAndCountAll({where: where}).then(function (res) {
-      return resolve(res.count === scadaIds.length);
-    }).catch(function (err) {
-      return reject(err);
+    _sequelize.query(sql.toString(), { type: _sequelize.QueryTypes.SELECT, model: userAllowDeviceVo }).then((raws) => {
+      resolve(raws);
+    }).catch((err) => {
+      reject(err);
     });
   });
 }
@@ -105,12 +198,11 @@ module.exports = {
   init: _init,
   getScadaList: _getScadaList,
   getScadaListByProjectId: _getScadaListByProjectId,
-  getScadaByProjectId: _getScadaByProjectId,
   getScada: _getScada,
   insertScada: _insertScada,
   updateScada: _updateScada,
   deleteScada: _deleteScada,
-  unbindScadas: _unbindScadas,
+  /* unbindScadas: _unbindScadas, */
   bindScadas: _bindScadas,
-  isScadaIdsAllExist: _isScadaIdsAllExist
+  checkScadaRightByUserName: _checkScadaRightByUserName
 };
