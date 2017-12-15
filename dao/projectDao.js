463da57bbb32dfb9703d19de2ddc038e2be09ae7
stacy.yeh <stacy.yeh@advantech.com.tw>
2017-12-15 14:39:19 +0800

    [Added] ScadaDao - checkProjectRightByUserName [Changed] - getProjectList - updateProject - deleteProject

--- dao/projectDao.js
+++ dao/projectDao.js
@@ -1,42 +1,70 @@
 'use strict';
 
-const Sequelize = require('sequelize');
+const Promise = require('bluebird');
 const Utils = require('../common/utils');
+var squel = require('squel').useFlavour('postgres');
 
 var projectVo = null;
 var scadaVo = null;
+var userAllowDeviceVo = null;
+var scadaDao = require('./scadaDao');
+var _sequelize = null;
 
 function _init (sequelize) {
   projectVo = sequelize.import('../models/projectVo');
   scadaVo = sequelize.import('../models/scadaVo');
+  userAllowDeviceVo = sequelize.import('../models/userAllowDeviceVo');
+  _sequelize = sequelize;
 }
 
 /**
  * @param {Object} filterObj
  * @param {Integer} filterObj.offset: starting index
  * @param {Integer} filterObj.limit: data retrived
- * @param {String} filterObj.projectName: filter project name
- * @param {String} filterObj.projectDesc: filter project desc
+ * @param {String} filterObj.projectId: filter project name
+ * @param {String} filterObj.description: filter project desc
  * @param {String} filterObj.sortby: sort properties
- * @param {String} filterObj.order: order asc or desc
+ * @param {String} filterObj.order: order asc or not
+ * @param {Boolean} filterObj.detail: select id & name only
+ * @param {String} filterObj.userName: filter the projects that userName can access
  *  }
  */
 
 function _getProjectList (filterObj = {}) {
-  let props = Object.keys(projectVo.attributes);
-  let where = {};
-  for (let idx in props) {
-    let key = props[idx];
-    if (!Utils.isNullOrUndefined(filterObj[key])) {
-      where[key] = { $like: filterObj[key] };
+  let offset = filterObj.offset ? filterObj.offset : 0;
+  let limit = filterObj.limit ? filterObj.limit : null;
+  let sortby = filterObj.sortby ? filterObj.sortby : 'projectId';
+  let order = filterObj.order ? filterObj.order : true;
+  let detail = filterObj.detail ? filterObj.detail : false;
+
+  let sql = squel.select().from('scada.project_list', 'Project');
+  if (detail) {
+    for (let idx in projectVo.attributes) {
+      sql.field('Project.' + projectVo.attributes[idx].field, idx);
+    }
+  } else {
+    sql.field('Project.proj_id', 'projectId');
+  }
+  sql.distinct();
+  if (filterObj.userName) {
+    sql.join('scada.user_allow_device', 'UserAllowDevice', squel.expr().and('Project.proj_id = UserAllowDevice.proj_id'));
+    sql.join('scada.user_info', 'UserInfo', squel.expr().and('UserAllowDevice.user_id = UserInfo.user_id'));
+    sql.where('Userinfo.user_name = ?', filterObj.userName);
+  }
+  for (let idx in projectVo.attributes) {
+    let key = projectVo.attributes[idx];
+    if (!Utils.isNullOrUndefined(filterObj[idx])) {
+      sql.where('Project.' + key.field + ' LIKE ?', filterObj[idx]);
     }
   }
-  let filter = {};
-  filter.offset = !Utils.isNullOrUndefined(filterObj.offset) ? filterObj.offset : null;
-  filter.limit = !Utils.isNullOrUndefined(filterObj.limit) ? filterObj.limit : null;
-  filter.order = !Utils.isNullOrUndefined(filterObj.sortby) && !Utils.isNullOrUndefined(filterObj.order) ? Sequelize.literal(filterObj.sortby + ' ' + filterObj.order) : null;
-  filter.where = where;
-  return projectVo.findAndCountAll(filter);
+  sql.order(sortby, order);
+  return new Promise((resolve, reject) => {
+    _sequelize.query(sql.toString(), { type: _sequelize.QueryTypes.SELECT, model: projectVo }).then((raws) => {
+      resolve({count: raws.length, rows: raws.slice(offset, limit ? limit + offset : raws.length)});
+    }).catch((err) => {
+      reject(err);
+    });
+  });
 }
 
 function _getProject (projectId) {
@@ -47,27 +75,51 @@ function _insertProject (project, trans) {
   return projectVo.create(project, { transaction: trans });
 }
 
-function _updateProjectById (projectId, project, trans) {
-  return projectVo.update(project, { where: { projectId } }, { transaction: trans }).then(function (c) {
+function _updateProject (projectId, project, trans) {
+  return projectVo.update(project, { where: { projectId }, transaction: trans }).then(function (c) {
     if (project.projectId && project.projectId !== projectId) {
-      return scadaVo.update({ projectId: project.projectId }, { where: { projectId: projectId } }, { transaction: trans });
+      return scadaVo.update({ projectId: project.projectId }, { where: { projectId }, transaction: trans }).then((result) => {
+        return userAllowDeviceVo.update({ projectId: project.projectId }, { where: { projectId }, transaction: trans });
+      });
     }
   });
 }
 
-function _deleteProjectById (projectId, trans) {
-  return projectVo.destroy({ where: { projectId }, transaction: trans }).then(function (rows) {
-    return scadaVo.update({projectId: null}, { where: { projectId } }, { transaction: trans });
+function _deleteProject (projectId, trans) {
+  let promises = [];
+  return scadaVo.findAll({where: {projectId}}).then(function (scadas) {
+    for (let idx in scadas) {
+      promises.push(scadaDao.deleteScada(scadas[idx].scadaId, trans));
+    }
+    promises.push(projectVo.destroy({ where: { projectId }, transaction: trans }));
+    promises.push(userAllowDeviceVo.destroy({ where: { projectId }, transaction: trans }));
+    return Promise.all(promises);
   });
 }
 
-function _isProjectIdsAllExist (projectIds = [], trans) {
-  let where = {projectId: {$or: projectIds}};
+function _checkProjectRightByUserName (userName, filterObj = {}) {
+  let sql = squel.select().from('scada.project_list', 'Project');
+  sql.field('Project.proj_id', 'projectId');
+  sql.field('SUB.userId', 'userId');
+  sql.distinct();
+  sql.left_join(squel.select().from('scada.user_allow_device', 'UserAllowDevice')
+  .field('UserAllowDevice.user_id', 'userId').field('UserAllowDevice.proj_id', 'projectId')
+  .join('scada.user_info', 'UserInfo', squel.expr().and('UserAllowDevice.user_id = UserInfo.user_id').and('Userinfo.user_name = ?', userName)), 'SUB', 'SUB.projectId = Project.proj_id');
+  if (filterObj.projectId) {
+    sql.where(('UserAllowDevice.proj_id = ?', filterObj.projectId));
+  }
+  if (filterObj.scadaId) {
+    sql.where(('UserAllowDevice.scada_id = ?', filterObj.scadaId));
+  }
+  if (filterObj.deviceId) {
+    sql.where(('UserAllowDevice.device_id = ?', filterObj.deviceId));
+  }
+
   return new Promise((resolve, reject) => {
-    projectVo.findAndCountAll({where: where}).then(function (res) {
-      return resolve(res.count === projectIds.length);
-    }).catch(function (err) {
-      return reject(err);
+    _sequelize.query(sql.toString(), { type: _sequelize.QueryTypes.SELECT, model: userAllowDeviceVo }).then((raws) => {
+      resolve(raws);
+    }).catch((err) => {
+      reject(err);
     });
   });
 }
@@ -77,7 +129,7 @@ module.exports = {
   getProjectList: _getProjectList,
   getProject: _getProject,
   insertProject: _insertProject,
-  updateProjectById: _updateProjectById,
-  deleteProjectById: _deleteProjectById,
-  isProjectIdsAllExist: _isProjectIdsAllExist
+  updateProject: _updateProject,
+  deleteProject: _deleteProject,
+  checkProjectRightByUserName: _checkProjectRightByUserName
 };
